9aeaff85dbcf6d2ec4b8da309837c4da85c363b2
lly <liu_lingyuan@icloud.com>
2023-08-08 07:45:30 +0000

    use fp16

--- finetune.py
+++ finetune.py
@@ -112,7 +112,7 @@ def train(
 
     model = LlamaForCausalLM.from_pretrained(
         base_model,
-        load_in_8bit=True,
+        # load_in_8bit=True,
         torch_dtype=torch.float16,
         device_map=device_map,
     )
